e8aaaa6dfc34f079c319ab7e05f1762dec7721d3
skyefactory <d.ostapa@gmail.com>
2026-01-27 21:49:47 -0600

    added simple random order system. rn all logic stored within the ticket printer (weird) and just outputs an order to the console at game start. Want to get this on a random timer btwn 1-2 mins so the player can get orders done. Need to add a actual ticket item that the player can pickup and view the generated order

--- scripts/reciept_printer.gd
+++ scripts/reciept_printer.gd
@@ -1,6 +1,40 @@
 extends Node3D
 @export var hasTicket: bool
 @onready var ticketPickupPrompt = $ticketpickup
+var orders = []
+#First entry is the "base" recipie, second entry is the "variant", need to pick
+var Recipies = [
+	["Burger", ["Cheese", "Normal", "Double"]],
+	["Beer", ["Corona", "Budweiser", "Coors Light"]]
+]
+
+func _ready():
+	var myOrder = placeOrder()
+	orders.append({
+		"ID": orders.size(),
+		"Order": myOrder
+	})
+	print(orders)
+
+
+func placeOrder() -> Array:
+	var order = [] #holds the order
+	
+	var numItems = randi() % 3 + 1 #number of items in this order
+	
+	for i in range(numItems):
+		var recipe_index = randi() % Recipies.size()
+		var recipe = Recipies[recipe_index]
+		
+		var variation_index = randi() % recipe[1].size()
+		var variation = recipe[1][variation_index]
+		
+		order.append({
+			"Base": recipe[0],
+			"Variation": variation
+		})
+		
+	return order
 
 func _on_trigger_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Players"):

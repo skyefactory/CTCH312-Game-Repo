ef00a7880a8096760928ec72545eaf8b0143ff72
skyefactory <d.ostapa@gmail.com>
2026-02-08 17:46:19 -0600

    Added simple FPS character, code adapted from https://github.com/rbarongr/GodotFirstPersonController/tree/main... Changed the layout of the kitchen a bit, instead of a reciept system there will be a large order screen, and the player can select their order to work on. right now I have added a plane with a subviewport that shows a list of mock orders while I work on the selecting logic.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 **Clocked In - A game designed for CTCH312**
-
+This game is made in GODOT
 * Simple time based cooking game. 
 * Manage your restaurant and prepare orders. 
 * Unlock new recipes and kitchen equipment. 
--- scenes/closegame.gd
+++ scenes/closegame.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
